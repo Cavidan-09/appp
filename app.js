7aab4031c4fc3d2a123c4d7a0c5a61d633a2bce1
Cavidan-09 <cavidanmesimli6@gmail.com>
2024-08-27 18:49:05 +0400

    first commit

---
+++ app.js
@@ -0,0 +1,7 @@
+let username = prompt("Nece yasiniz var ")
+let user = username.split("")
+console.log(username);
+let text = ("icaze verilmedi")
+console.log(text)
+let code = ("icaze verildi")
+console.log("icaze verildi")
